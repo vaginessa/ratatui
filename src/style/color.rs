d0245ab477ebec4c279c0f7a4304be8b6621c0c8
Josh McKinney <joshka@users.noreply.github.com>
2024-04-26 00:36:32 -0700

    fix: cargo lint sorting and color parsing

--- src/style/color.rs
+++ src/style/color.rs
@@ -316,17 +316,20 @@ impl FromStr for Color {
                 _ => {
                     if let Ok(index) = s.parse::<u8>() {
                         Self::Indexed(index)
-                    } else if let (Ok(r), Ok(g), Ok(b)) = {
-                        if !s.starts_with('#') || s.len() != 7 {
-                            return Err(ParseColorError);
-                        }
-                        (
-                            u8::from_str_radix(&s.chars().skip(1).take(2).collect::<String>(), 16),
-                            u8::from_str_radix(&s.chars().skip(3).take(2).collect::<String>(), 16),
-                            u8::from_str_radix(&s.chars().skip(5).take(2).collect::<String>(), 16),
-                        )
-                    } {
-                        Self::Rgb(r, g, b)
+                    } else if s.starts_with('#') && s.len() == 7 {
+                        let red = s
+                            .get(1..3)
+                            .and_then(|v| u8::from_str_radix(v, 16).ok())
+                            .ok_or(ParseColorError)?;
+                        let green = s
+                            .get(3..5)
+                            .and_then(|v| u8::from_str_radix(v, 16).ok())
+                            .ok_or(ParseColorError)?;
+                        let blue = s
+                            .get(5..7)
+                            .and_then(|v| u8::from_str_radix(v, 16).ok())
+                            .ok_or(ParseColorError)?;
+                        Self::Rgb(red, green, blue)
                     } else {
                         return Err(ParseColorError);
                     }
